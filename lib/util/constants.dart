f9ff63a55f6ecd74ec3bd756ec37391eea8f086a
andrei.munteanu <andrei.munteanu@snt.ro>
2022-11-06 20:53:57 +0200

    feat: login with google account error: 400 redirect_uri_mismatch

---
+++ lib/util/constants.dart
@@ -0,0 +1,27 @@
+import 'dart:io' show Platform;
+
+const REFRESH_TOKEN_KEY = 'refresh_token';
+const BACKEND_TOKEN_KEY = 'backend_token';
+const GOOGLE_ISSUER = 'https://accounts.google.com';
+const GOOGLE_CLIENT_ID_IOS = '125789040129-62uibo24q827792gnd846cc1jdoo752s.apps.googleusercontent.com';
+const GOOGLE_REDIRECT_URI_IOS = 'com.googleusercontent.apps.125789040129-62uibo24q827792gnd846cc1jdoo752s.apps.googleusercontent.com:/oauthredirect';
+const GOOGLE_CLIENT_ID_ANDROID = '125789040129-i90b31ck9jagtob63ts73ntpnfvh7at7.apps.googleusercontent.com';
+const GOOGLE_REDIRECT_URI_ANDROID = 'com.googleusercontent.apps.125789040129-i90b31ck9jagtob63ts73ntpnfvh7at7.apps.googleusercontent.com:/oauthredirect';
+
+String clientID() {
+  if(Platform.isAndroid) {
+    return GOOGLE_CLIENT_ID_ANDROID;
+  } else if (Platform.isIOS) {
+    return GOOGLE_CLIENT_ID_IOS;
+  }
+  return '';
+}
+
+String redirectUrl() {
+  if(Platform.isAndroid) {
+    return GOOGLE_REDIRECT_URI_ANDROID;
+  } else if (Platform.isIOS) {
+    return GOOGLE_REDIRECT_URI_IOS;
+  }
+  return '';
+}
