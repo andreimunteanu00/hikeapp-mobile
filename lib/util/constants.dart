73901991e63bf7f1b921880a87b0577ec40c5c2c
andrei.munteanu <andrei.munteanu@snt.ro>
2022-11-06 21:22:10 +0200

    fix: 400 redirect_uri_mismatch error: redirect failed back to app

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -4,9 +4,9 @@ const REFRESH_TOKEN_KEY = 'refresh_token';
 const BACKEND_TOKEN_KEY = 'backend_token';
 const GOOGLE_ISSUER = 'https://accounts.google.com';
 const GOOGLE_CLIENT_ID_IOS = '125789040129-62uibo24q827792gnd846cc1jdoo752s.apps.googleusercontent.com';
-const GOOGLE_REDIRECT_URI_IOS = 'com.googleusercontent.apps.125789040129-62uibo24q827792gnd846cc1jdoo752s.apps.googleusercontent.com:/oauthredirect';
+const GOOGLE_REDIRECT_URI_IOS = 'com.example.hikeappmobile:/oauthredirect';
 const GOOGLE_CLIENT_ID_ANDROID = '125789040129-i90b31ck9jagtob63ts73ntpnfvh7at7.apps.googleusercontent.com';
-const GOOGLE_REDIRECT_URI_ANDROID = 'com.googleusercontent.apps.125789040129-i90b31ck9jagtob63ts73ntpnfvh7at7.apps.googleusercontent.com:/oauthredirect';
+const GOOGLE_REDIRECT_URI_ANDROID = 'com.example.hikeappmobile:/oauthredirect';
 
 String clientID() {
   if(Platform.isAndroid) {
