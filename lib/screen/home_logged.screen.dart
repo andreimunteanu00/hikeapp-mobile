4e27de5d0d6b57a767f889e6a734c5789a2bf970
andrei.munteanu <andrei.munteanu@snt.ro>
2023-02-20 22:10:32 +0200

    feat: check suspended account

--- lib/screen/home_logged.screen.dart
+++ lib/screen/home_logged.screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'log_in_screen.dart';
+import 'package:hikeappmobile/widget/sign_out.widget.dart';
 
 class HomeLoggedScreen extends StatefulWidget {
 
@@ -12,52 +11,14 @@ class HomeLoggedScreen extends StatefulWidget {
 }
 
 class HomeLoggedScreenState extends State<HomeLoggedScreen> {
-  int _currentIndex = 0;
-
-  final List<Widget> _children = [
-    const LogInScreen(),
-  ];
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        children: [
-          // Main content
-          _children[_currentIndex],
-
-          // Bottom navigation bar
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: BottomNavigationBar(
-              currentIndex: _currentIndex,
-              onTap: onTabTapped,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-                  label: 'Search',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.settings),
-                  label: 'Settings',
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        const Text('logged!'),
+        SignOutWidget()
+      ],
     );
   }
 }
